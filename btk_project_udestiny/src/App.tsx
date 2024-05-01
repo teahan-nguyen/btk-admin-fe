095f2d6ee2c0d9f94fbcef918f0d228063df2fe1
teahan105 <thanhnd@vmogroup.com>
2024-05-01 23:18:15 +0700

    Change monorepo name and add scripts to root by using bun workspace

--- btk_project_udestiny/src/App.tsx
+++ btk_project_udestiny/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import React from "react";
 import AdminService from "common_admin/app";
 import CMSCommonService from "common_services/cms";
+import BlogCommonService from "common_services/blog";
 // const AdminService = React.lazy(() => import('common_admin/app'));
 // const CMSCommonService = React.lazy(() => import('common_services/cms'));
 
@@ -9,7 +10,7 @@ const App = () => {
     return (
         <div className="content">
             <h1>Udestiny</h1>
-            <AdminService children={<CMSCommonService/>}/>
+            <AdminService children={<><CMSCommonService/><BlogCommonService/></>}/>
         </div>
     );
 };
